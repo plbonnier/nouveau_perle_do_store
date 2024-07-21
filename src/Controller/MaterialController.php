b55b4b571ee8503e3d518f28dd7f6170a78d945f
plbonnier <pl_bonnier@yahoo.fr>
2024-07-21 17:18:51 +0200

    entiry, index, edit and new done. Need to have product for the link and the road

---
+++ src/Controller/MaterialController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Material;
+use App\Form\MaterialType;
+use App\Repository\MaterialRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/material')]
+class MaterialController extends AbstractController
+{
+    #[Route('/', name: 'app_material_index', methods: ['GET'])]
+    public function index(MaterialRepository $materialRepository): Response
+    {
+        return $this->render('material/index.html.twig', [
+            'materials' => $materialRepository->findBy([], ['name' => 'ASC']),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_material_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $material = new Material();
+        $form = $this->createForm(MaterialType::class, $material);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($material);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_material_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('material/new.html.twig', [
+            'material' => $material,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_material_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Material $material, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(MaterialType::class, $material);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_material_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('material/edit.html.twig', [
+            'material' => $material,
+            'form' => $form,
+        ]);
+    }
+}
---
+++ src/DataFixtures/MaterialFixtures.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Material;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class MaterialFixtures extends Fixture
+{
+    public const MATERIALS = [
+        ['name' => 'UNAKITE'],
+        ['name' => 'LABRADORITE'],
+        ['name' => 'CALCITE'],
+        ['name' => 'MALACHITE'],
+        ['name' => 'OEIL DE TIGRE'],
+        ['name' => 'ONYX'],
+        ['name' => 'AMETHYSTE'],
+        ['name' => 'ANGELITE'],
+        ['name' => 'QUARTZ ROSE'],
+        ['name' => 'TOURMALINE NOIRE'],
+        ['name' => 'LAPIS LAZULI'],
+        ['name' => 'CRISTAL DE ROCHE'],
+        ['name' => 'AGATE'],
+        ['name' => 'AGATE DU BOTSWANA'],
+        ['name' => 'SERPENTINE'],
+        ['name' => 'RHODONITE'],
+        ['name' => 'AMETHYSTE ZONEE'],
+        ['name' => 'AVENTURINE VERTE'],
+        ['name' => 'JASPE'],
+        ['name' => 'DUMORTIERITE'],
+        ['name' => 'HOWLITE'],
+        ['name' => 'OBSIDIENNE DES NEIGES'],
+        ['name' => 'OPALE'],
+        ['name' => 'HEMATITE'],
+        ['name' => 'QUARTZ'],
+        ['name' => 'PIERRE DE LUNE'],
+        ['name' => 'CHRYSOCOLLE'],
+        ['name' => 'OEIL DE LUCIE'],
+        ['name' => 'FELDSPATH'],
+        ['name' => 'CELESTINE'],
+        ['name' => 'SELENITE'],
+        ['name' => 'AUTRES'],
+        ['name' => 'CORNALINE'],
+        ['name' => 'SEL HIMALAYA'],
+        ['name' => 'PERLE'],
+        ['name' => 'OEIL DE TAUREAU'],
+        ['name' => 'NACRE'],
+        ['name' => 'PREHNITE'],
+        ['name' => 'TURQUOISE'],
+        ['name' => 'AMAZONITE'],
+        ['name' => 'GRENAT'],
+        ['name' => 'KUNZITE'],
+        ['name' => 'SODALITE'],
+        ['name' => 'CITRINE'],
+        ['name' => 'AIGUE MARINE'],
+        ['name' => 'JADE'],
+        ['name' => 'MULTIPIERRES'],
+        ['name' => 'APATITE'],
+        ['name' => 'PIERRE DE SOLEIL'],
+        ['name' => 'ONYX INDIEN'],
+        ['name' => 'AMBRE'],
+        ['name' => 'LAVE'],
+        ['name' => 'TANZANITE'],
+        ['name' => 'BOIS FOSSILE'],
+        ['name' => 'FLUORITE'],
+        ['name' => 'APPRET'],
+        ['name' => 'APPRET'],
+        ['name' => 'RHYOLITE'],
+        ['name' => 'AGATE MOUSSE'],
+        ['name' => 'LEPIDOLITE'],
+        ['name' => 'LEPIDOLITE'],
+        ['name' => 'RHODOCHROSITE'],
+        ['name' => 'HOWLITE'],
+        ['name' => 'CHAINE AG'],
+        ['name' => 'CHAINE AG'],
+        ['name' => 'PALO SANTO'],
+        ['name' => 'ANGE'],
+        ['name' => 'SHUNGITE'],
+        ['name' => 'HEMATITE'],
+        ['name' => 'FORFAIT REPARATION'],
+        ['name' => 'TOURMALINE'],
+    ];
+
+    public function load(ObjectManager $manager): void
+    {
+        foreach (self::MATERIALS as $materialFixture) {
+            $material = new Material();
+            $material->setName($materialFixture['name']);
+            $manager->persist($material);
+        }
+
+        $manager->flush();
+    }
+}
