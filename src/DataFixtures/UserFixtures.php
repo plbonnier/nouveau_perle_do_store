09d352fe481cce8dddda079d9d25bda80d6128f4
plbonnier <pl_bonnier@yahoo.fr>
2025-01-30 18:31:20 +0100

    enlevage des fixtures sauf pour user, rajout sur produit d'une info sur la tva

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -19,13 +19,13 @@ class UserFixtures extends Fixture
         $user = new User();
         $user->setUsername('user');
         $user->setRoles(['ROLE_USER']);
-        $user->setPassword($this->passwordHasher->hashPassword($user, 'user'));
+        $user->setPassword($this->passwordHasher->hashPassword($user, 'User*1234'));
         $manager->persist($user);
 
         $admin = new User();
         $admin->setUsername('admin');
         $admin->setRoles(['ROLE_ADMIN']);
-        $admin->setPassword($this->passwordHasher->hashPassword($admin, 'admin'));
+        $admin->setPassword($this->passwordHasher->hashPassword($admin, 'Admin*1234'));
         $manager->persist($admin);
         
         $manager->flush();
